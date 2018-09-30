fa17760efe2e62ce67946e60fe9846ffdabb18bd
naitik0212 <shahnaitik0212@gmail.com>
2018-09-29 17:21:32 -0700

    adding latest changes

--- src/components/app.js
+++ src/components/app.js
@@ -5,7 +5,17 @@ import WeatherList from "../container/weather_list";
 export default class App extends Component {
   render() {
     return (
-      <div><br/><SearchBar />
+      <div style={{ background: 'linear-gradient(to right bottom, white, snow, #bdc3c7, #d7d2cc, #304352)',textAlign: "center"}}>
+          <br />
+        <div >
+
+            <div style={{ background: 'linear-gradient(to right bottom, #430089, #82ffa1)', minHeight: "120px",textAlign: "center"}}>
+                <h2 style={{ padding: "10px 20px", textAlign: "center", color: "white"}}>Welcome to the Weather Application</h2>
+                <h5>Type the city name to find the weather details:</h5>
+
+            </div>
+        </div>
+        <SearchBar />
          <WeatherList />
       </div>
 
--- src/config.js
+++
@@ -1,3 +0,0 @@
-const config = {
-    API_KEY : '011b0a249a9aa6cffe438b1d79228a7b',
-};
